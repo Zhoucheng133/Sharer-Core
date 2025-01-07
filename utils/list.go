9ad3f44836d4c6d85a58505f9886fbade01244b9
zhoucheng <zhoucheng133@outlook.com>
2025-01-07 19:43:26 +0800

    添加获取文件列表

--- utils/list.go
+++ utils/list.go
@@ -1,11 +1,60 @@
 package utils
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 type Body struct {
 	Path string `json:"path"`
+}
+
+type Item struct {
+	Name  string `json:"name"`
+	Size  int64  `json:"size"`
+	IsDir bool   `json:"isDir"`
+}
+
+type RequestResponse struct {
+	Ok    bool   `json:"ok"`
+	Items []Item `json:"items"`
+	Msg   string `json:"msg"`
+}
+
+func listHandler(path string) RequestResponse {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return RequestResponse{
+			Ok:    false,
+			Items: nil,
+			Msg:   "Path does not exist or is not accessible",
+		}
+	}
+	var items []Item
+	for _, file := range files {
+		fileInfo, err := file.Info()
+		if err != nil {
+			continue
+		}
+		var size int64
+		if file.IsDir() {
+			size = 0
+		} else {
+			size = fileInfo.Size()
+		}
+		items = append(items, Item{
+			Name:  file.Name(),
+			IsDir: file.IsDir(),
+			Size:  size,
+		})
+	}
+
+	return RequestResponse{
+		Ok:    true,
+		Items: items,
+		Msg:   "",
+	}
 }
 
 func GetList(c *gin.Context) {
@@ -18,10 +67,7 @@ func GetList(c *gin.Context) {
 				"msg": "Path parameter is required",
 			})
 		} else {
-			c.JSON(200, gin.H{
-				"ok":  true,
-				"msg": body.Path,
-			})
+			c.JSON(200, listHandler(body.Path))
 		}
 		return
 	}
