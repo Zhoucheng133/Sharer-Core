2cb104fb6f93af1a7a1c3c0b9dae353a45422210
zhoucheng <zhoucheng133@outlook.com>
2025-06-27 23:12:02 +0800

    将文件内嵌入动态库

--- utils/static.go
+++ utils/static.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"mime"
 	"net/http"
-	"os"
 	"path/filepath"
 	"strings"
 
@@ -29,22 +28,4 @@ func StaticHandler(c *gin.Context, f embed.FS) {
 	contentType := mime.TypeByExtension(ext)
 	c.Header("Content-Type", contentType)
 	c.DataFromReader(http.StatusOK, -1, path, file, map[string]string{})
-}
-
-func DynamicLibStaticHandler(c *gin.Context, basePath string) {
-	path := strings.TrimPrefix(c.Request.URL.Path, "/")
-	if path == "" || path == "/" || strings.HasPrefix(path, "login") {
-		path = "index.html"
-	}
-
-	fullPath := filepath.Join(basePath, path)
-
-	// 检查文件是否存在
-	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
-		c.JSON(http.StatusNotFound, gin.H{"error": "File not found: " + fullPath})
-		return
-	}
-
-	// 直接返回文件
-	c.File(fullPath)
 }
